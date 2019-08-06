9b9e677e3b0074fc50d34bb8616cb4365a821dfb
aga888 <aggusia@onet.pl>
2019-08-06 21:34:14 +0200

    Add returnName method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
-        System.out.println(returnAge());
 
+        System.out.println(returnAge());
+        System.out.println(returnName());
     }
 
     static int returnAge() {
         return 21;
+    }
+
+    static String returnName() {
+        return "Aga";
     }
 
 }
